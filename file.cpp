974713adce306a529c11d569e2fcb5a26ea136a9
yuvrajsidhu08 <454009@bsd48.org>
2025-11-02 23:48:14 -0800

    file

--- file.cpp
+++ file.cpp
@@ -12,27 +12,36 @@ using namespace std;
 struct Student { char first[50], last[50]; int id; float gpa; };
 
 int main() {
+
+  // stores student pointers
     vector<Student*> v; char cmd[10];
     while (true) {
         cout << "Command (ADD, PRINT, DELETE, QUIT): ";
         cin >> cmd;
+
+	// Add a new student
 	if (!strcmp(cmd, "ADD")) {
             auto* s = new Student;
             cout << "First Last ID GPA: ";
             cin >> s->first >> s->last >> s->id >> s->gpa;
             v.push_back(s);
-        } 
+        }
+
+	// Print all students	
         else if (!strcmp(cmd, "PRINT")) {
             cout << fixed << setprecision(2);
             for (auto* s : v)
                 cout << s->first << " " << s->last << ", " << s->id << ", " << s->gpa << endl;
         }
+	// delete by the ID
 	else if (!strcmp(cmd, "DELETE")) {
             int id; cout << "ID: "; cin >> id;
             for (auto it = v.begin(); it != v.end(); ++it)
                 if ((*it)->id == id) { delete *it; v.erase(it); break; }
-        } 
-        else if (!strcmp(cmd, "QUIT")) {
+        }
+	
+	// Free memory and then quit
+	else if (!strcmp(cmd, "QUIT")) {
             for (auto* s : v) delete s;
             break;
         }
